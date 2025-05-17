f1d8dea532ab1a54f3957a0ad506fec47b9104cb
Sammy2141 <samkit681@icloud.com>
2025-05-17 13:27:08 +0530

    Update video30.py

---
+++ video30.py
@@ -0,0 +1,11 @@
+def five(x):
+    x = 5
+    return x
+    print(five(3))
+print(five(3))
+# Using indentation, write a function called ten() that takes any argument x, assigns the value 10 to x, and then returns x.
+# Call the function with an argument of 3 and print the result.
+def ten(x):
+    x = 10
+    return x
+print(ten(3))
