e0ec3918ab38e49b419eb79cb4be76353687a703
Sammy2141 <samkit681@icloud.com>
2025-05-17 13:07:07 +0530

    Update video28.py

---
+++ video28.py
@@ -0,0 +1,10 @@
+print(2.0 * 1.5 + 5)
+result = (2.0 * 1.5 + \
++ 5)
+print(result)
+# Add a backslash in the code below, so it is a one-line code. Observe the change in the result.
+# 15 + 31
+# - 26
+result = 15 + 31 \
+- 26
+print(result)
