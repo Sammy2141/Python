9bf1da4c567b95f4c6d19708d56adb07a3527c22
Sammy2141 <samkit681@icloud.com>
2025-05-17 14:43:00 +0530

    Update video31.py

---
+++ video31.py
@@ -0,0 +1,18 @@
+print(10 == 20 / 2)
+print(10 == 20 // 2)
+print(10 != 20 / 2)
+print(10 != 20 // 2)
+print(10 != 10)
+print(10 != 15)
+print(100 > 50)
+print(100 < 50)
+print(15 >= 10 + 10)
+print(15 <= 10 + 5)
+# Verify that 25 is smaller than 30.
+print(25 < 30)
+# Verify that 5 multiplied by 3 is less than or equal to 5 to the power of 3.
+print(5 * 3 <= 5 ** 3)
+# Verify that 100 is equal to 10 square.
+print(100 == 10 ** 2)
+# Verify that 53 is not equal to 46.
+print(53 != 46)
