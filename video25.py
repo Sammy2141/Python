7419c894d4538d0b3cc1e4d4b166ff5d74194501
Sammy2141 <samkit681@icloud.com>
2025-05-17 12:31:20 +0530

    Update video25.py

--- video25.py
+++ video25.py
@@ -3,3 +3,6 @@ print(y)
 print(y==125)
 print(y==125.0)
 print(y==126)
+
+# Use a double-equality sign (==) to check if 100 equals 98. Display the result without using the print() function.
+print(100==98)
