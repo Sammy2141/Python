3292922e89574251c2e60c2ba252a65843f3505f
Sammy2141 <samkit681@icloud.com>
2025-05-17 12:31:56 +0530

    Reassign Values
    Reassign Values

---
+++ video26.py
@@ -0,0 +1,17 @@
+z = 1
+print(z)
+z = 3
+print(z)
+print(z+5)
+z = 7
+print(z)
+# Assign the value of 14 to a variable p.
+p = 14
+# Assign the value of 14 to a variable p.
+# Calculate p + 10.
+p = 14
+print(p + 10)
+# Now, assign 30 to the variable p.
+p = 30
+# Calculate p + 10.
+print(p + 10)
