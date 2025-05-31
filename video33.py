7b78a318c83e7caf5d7f0ccc9c6cc9de37bdaec3
Sammy2141 <samkit681@icloud.com>
2025-05-31 11:24:57 +0530

    IF Statement
    IF Statement

---
+++ video33.py
@@ -0,0 +1,18 @@
+if 5 == 15 / 3:
+    print("Horray!")
+if 5 == 18 / 3:
+    print("Horray!")
+if 5 == 20 / 4:
+    print("Horray!")
+if 5 != 3 * 6:
+    print("Horray!")
+# Create a two-line code that prints "The condition has been satisfied" if 5 is greater than 2.
+if 5 > 2:
+    print("The condition has been satisfied")
+# Assign 10 to the variable x and 25 to the variable y. In the same cell, create 2 conditional statements. Let the first one print "Both conditions are correct" if x is greater then 3 and y is greater than 13. Let the second one print "At least one of the conditions is false" if x is less than or equal to 3 or y is less than or equal to 13. Change the values assigned to x and y and re-run the cell to verify your code still works.
+x = 10
+y = 25
+if x > 3 and y > 13:
+    print("Both conditions are correct")
+if x <= 3 or y <= 13:
+    print("At least one of the conditions is false")
