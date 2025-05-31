1fdd15d6b3de1ddbc4bf77189f3a95e0ce99ccd7
Sammy2141 <samkit681@icloud.com>
2025-05-31 11:39:24 +0530

    ELSE Statement
    ELSE Statement

---
+++ video34.py
@@ -0,0 +1,11 @@
+x = 1
+if x > 3:
+    print("Case 1")
+else:
+    print("Case 2")
+# Let x represent the number of orders received during a certain day. Create a program that prints "A busy day" if x is greater than 100, and "A calm day" otherwise
+x = 120
+if x > 100:
+    print("A busy day")
+else:
+    print("A calm day")
