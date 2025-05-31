bbe2831a1a4b682d472ca834b6faa5e81a7ce34b
Sammy2141 <samkit681@icloud.com>
2025-05-31 12:21:51 +0530

    v1.py
    v1.py

--- v1.py
+++ v1.py
@@ -1,2 +1,11 @@
 print("Hello This is Samkit")
 print("Click", '"OK"')
+def compare_to_five(y):
+    if y > 5:
+        return "Greater"
+    elif y < 5:
+        return "Less"
+    else:
+        return "Equal"
+    
+print(compare_to_five(10))
